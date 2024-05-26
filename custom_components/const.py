7f98df264e9519f9ecd297455bd42154c1348839
chipsi <30642227+chipsi@users.noreply.github.com>
2024-05-26 07:01:54 +0200

    Create const.py

---
+++ custom_components/const.py
@@ -0,0 +1,9 @@
+from homeassistant.const import Platform
+
+DOMAIN = "distribuce24"
+
+PLATFORMS = [Platform.SENSOR]
+
+
+ADDITIONAL_EXPORT_ELECTRICITY = 'additional_export_electricity'
+ADDITIONAL_IMPORT_ELECTRICITY = 'additional_import_electricity'
