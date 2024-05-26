b18ab4e3b4e920454802c16412f6d296519c811a
chipsi <30642227+chipsi@users.noreply.github.com>
2024-05-26 06:59:05 +0200

    Create __init__.py

---
+++ custom_components/distribuce24/__init__.py
@@ -0,0 +1,53 @@
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_CURRENCY, CONF_UNIT_OF_MEASUREMENT, STATE_ON, STATE_OFF
+from homeassistant.helpers.typing import ConfigType
+
+from .const import DOMAIN, PLATFORMS
+
+logger = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Setup our skeleton component."""
+    hass.data[DOMAIN] = {}
+    return True
+
+
+async def options_update_listener(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Handle options update."""
+    logger.debug('options_update_listener', config_entry.data)
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    logger.debug('async_setup_entry %s data: [%s]; options: [%s]', config_entry.unique_id, config_entry.data, config_entry.options)
+
+    hass.data[DOMAIN][config_entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    hass_data = dict(config_entry.data)
+    # Registers update listener to update config entry when options are updated.
+    unsub_options_update_listener = config_entry.add_update_listener(options_update_listener)
+    # Store a reference to the unsubscribe function to cleanup if an entry is unloaded.
+    hass_data["unsub_options_update_listener"] = unsub_options_update_listener
+    hass.data[DOMAIN][config_entry.entry_id] = hass_data
+
+    config_entry.async_on_unload(config_entry.add_update_listener(async_reload_entry))
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Unload config entry."""
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+
+
+async def async_reload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Reload config entry."""
+    logger.debug('async_reload_entry %s data: %s; options: %s', config_entry.unique_id, config_entry.data, config_entry.options)
+
+    await hass.config_entries.async_reload(config_entry.entry_id)
